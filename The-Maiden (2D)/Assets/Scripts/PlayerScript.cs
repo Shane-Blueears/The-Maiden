f0e25d79c82aa679397e33c3066d8adffc82e93c
Zen Shane Chia <zenshanec@gmail.com>
2019-09-21 17:07:43 -0600

    Movement and jump added

--- The-Maiden (2D)/Assets/Scripts/PlayerScript.cs
+++ The-Maiden (2D)/Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,22 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //movement setup
+        Vector3 movement = new Vector3();
+        movement.x = Input.GetAxis("Horizontal");
+
+        //jump setup
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movement.y = 5 * speed;
+        }
+
+        //Dash setup
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movement.x = 10 * speed;
+        }
+        //Move the player
+        this.GetComponent<Rigidbody>().AddForce(movement * speed);
     }
 }
