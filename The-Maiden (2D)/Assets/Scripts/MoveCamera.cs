cf52117a38d3953ee13343b43e59c9a321f85470
Zen Shane Chia <zenshanec@gmail.com>
2019-09-23 19:01:38 -0600

    Dash temporary completed

---
+++ The-Maiden (2D)/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject Player;
+    public float cameraDelay = 0.01f;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = (this.transform.position - Player.transform.position);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+        //transform.position = Vector3.Lerp(transform.position, Player.transform.position+offset, cameraDelay);
+    }
+}
